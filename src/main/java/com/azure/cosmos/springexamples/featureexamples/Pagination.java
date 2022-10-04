a3292bf110993b142710afd47a5c104680fc5360
Trevor Anderson <trande@microsoft.com>
2022-10-04 19:59:00 -0400

    Adding licensing header.

--- src/main/java/com/azure/cosmos/springexamples/featureexamples/Pagination.java
+++ src/main/java/com/azure/cosmos/springexamples/featureexamples/Pagination.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// package com.azure.cosmos.springexamples.featureexamples;
+package com.azure.cosmos.springexamples.featureexamples;
 
 import com.azure.cosmos.springexamples.common.User;
 import com.azure.cosmos.springexamples.quickstart.sync.UserRepository;
